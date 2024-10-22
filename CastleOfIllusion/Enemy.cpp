02fc6ad98bda4ee20ca864aea4f65b4c0b583ecf
Jon <jon.campillo@gmail.com>
2024-10-22 13:15:14 +0200

    Skill issue #3 Switch statement with if inside needs {}

--- CastleOfIllusion/Enemy.cpp
+++ CastleOfIllusion/Enemy.cpp
@@ -5,13 +5,15 @@ void Enemy::collideWithEntity(Collision collision)
 {
 	switch (collision.entity->getType()) 
 	{
-	case EntityType::Player:
+	case EntityType::Player: 
+	{
 		auto player = static_cast<Player*>(collision.entity);
-		if (player->isAttacking()) 
+		if (player->isAttacking())
 		{
 			// Die
 		}
 		break;
+	}
 	default:
 		break;
 	}
